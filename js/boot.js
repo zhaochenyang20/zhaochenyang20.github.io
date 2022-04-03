42fd61f95610b706e11474762958501f3a0d7d8f
zhaochen20 <zhaochenyang20@gmail.com>
2022-04-03 18:12:22 +0800

    Site updated: 2022-04-03 18:12:21

---
+++ js/boot.js
@@ -0,0 +1,16 @@
+/* global Fluid */
+
+Fluid.boot = {};
+
+Fluid.boot.registerEvents = function() {
+  Fluid.events.billboard();
+  Fluid.events.registerNavbarEvent();
+  Fluid.events.registerParallaxEvent();
+  Fluid.events.registerScrollDownArrowEvent();
+  Fluid.events.registerScrollTopArrowEvent();
+  Fluid.events.registerImageLoadedEvent();
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  Fluid.boot.registerEvents();
+});
